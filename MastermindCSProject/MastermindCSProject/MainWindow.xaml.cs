b9defcbace767f556f076f852be2ea0cf370ccab
FJanssen92 <12402449@student.pxl.be>
2024-11-04 14:46:19 +0100

    Mastermind-02 Code

--- MastermindCSProject/MastermindCSProject/MainWindow.xaml.cs
+++ MastermindCSProject/MastermindCSProject/MainWindow.xaml.cs
@@ -19,6 +19,53 @@ namespace MastermindCSProject
         public MainWindow()
         {
             InitializeComponent();
+
+            Title = "Mastermind " + "(" + RandomColors() + ")";
+
+           
+
         }
+
+        public string RandomColors()
+        {
+            Random newColor = new Random();
+            string randomColor;
+            StringBuilder colorCode = new StringBuilder();
+
+
+
+            for (int i = 1; i <= 4; i++)
+            {
+                randomColor = (newColor.Next(1, 7)).ToString();
+                switch (randomColor)
+                {
+                    case "1":
+                        randomColor = "White";
+                        break;
+                    case "2":
+                        randomColor = "Red";
+                        break;
+                    case "3":
+                        randomColor = "Blue";
+                        break;
+                    case "4":
+                        randomColor = "Green";
+                        break;
+                    case "5":
+                        randomColor = "Yellow";
+                        break;
+                    case "6":
+                        randomColor = "Orange";
+                        break;
+                }
+
+                colorCode.Append($"{randomColor}, ");
+
+                }
+
+            string colors = colorCode.ToString();
+            return colors;
+
+            }
     }
-}
+    }
